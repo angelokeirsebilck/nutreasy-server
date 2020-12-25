3094766d9720e26b5c8b168308f45620ef59ae50
angelokeirsebilck <angelo.keirsebilck1@telenet.be>
2020-12-25 12:10:02 +0100

    Init

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+app.use(
+  express.json({
+    extended: false,
+  })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server up and running on port ${PORT}`);
+});
---
+++ routes/api/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
