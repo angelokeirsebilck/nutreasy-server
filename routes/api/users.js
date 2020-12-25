7c7f5e06aa33c2d96d9dd47a753f8913211b772b
angelokeirsebilck <angelo.keirsebilck1@telenet.be>
2020-12-25 12:16:47 +0100

    Add post user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,3 +7,75 @@ const config = require('config');
 const auth = require('../../middleware/auth');
 
 const User = require('../../models/User');
+
+// @route   POST api/users
+// @desc    Register user
+// @access  Public
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send({
+        errors: errors.array(),
+      });
+    }
+
+    let { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({
+        email,
+      });
+
+      if (user) {
+        return res.status(400).send({
+          errors: [{ msg: 'User already exist.' }],
+        });
+      }
+
+      user = new User({
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (error, token) => {
+          if (error) {
+            throw error;
+          }
+          res.send({ token });
+        }
+      );
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send(error.message);
+    }
+  }
+);
+
+module.exports = router;
