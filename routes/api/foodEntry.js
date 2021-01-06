f6b0a2a138ef0ebc3ab426906951ece3b0258010
angelokeirsebilck <angelo.keirsebilck1@telenet.be>
2021-01-06 13:26:59 +0100

    Fix

--- routes/api/foodEntry.js
+++ routes/api/foodEntry.js
@@ -123,7 +123,7 @@ router.post(
 // @route   GET api/foodEntry
 // @desc    Get foodEntries for specific day
 // @access  Private
-router.get('/', auth, async (req, res) => {
+router.post('/', auth, async (req, res) => {
   // Destructure the request
   const { date } = req.body;
   //   let formatDate = moment(date, 'YYYY-MM-DD')
